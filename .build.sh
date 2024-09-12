10db915cbbe4445c8a76187302ce9198f995dfcd
Brett <brett.whynot@gmail.com>
2024-09-12 13:33:08 +1000

    v1.0.1

--- .build.sh
+++ .build.sh
@@ -3,7 +3,9 @@ cd "${0%/*}"
 OUTPUT="${1:-TA-tcpcheck.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
+rm -rf lib/*/__pycache__
+rm -rf lib/*/*/__pycache__
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite TA-tcpcheck 
