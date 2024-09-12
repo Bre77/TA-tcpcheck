83d2bc3e1b2e88b83a1a45485bb2dc1d7faba22f
Brett <brett.whynot@gmail.com>
2024-09-12 11:26:14 +1000

    Update build

--- .build.sh
+++ .build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd "${0%/*}"
 OUTPUT="${1:-TA-tcpcheck.spl}"
-pip install --upgrade --no-dependencies -t lib -r lib/requirements.txt
+chmod -R u=rwX,go= *
+chmod -R u-x+X *
+chmod -R u=rwx,go= *
+python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite TA-tcpcheck 
